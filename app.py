a866085508255289c8e49e2099c057031ebc050b
EstebanCardenas14 <lecardenasc@unbosque.edu.co>
2023-11-07 11:09:36 -0500

    cors 2

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 from flask import Flask
 from routes.user_routes import user_routes
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 
 # Carga las variables de entorno desde el archivo .env
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(user_routes)
-#cors 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', '*')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    return response
+
+
 
 @app.route('/')
 
